313d649203db923f72c7c014bf46bdf7689f4c38
Jlouisa <jonathan.louisa@gmail.com>
2023-08-15 12:35:29 +0200

    Add remove boolean to class

--- src/dev.js
+++ src/dev.js
@@ -1,10 +1,14 @@
 class CreateTodo {
-  constructor(title, description, dueDate, priority, notes) {
+  constructor(title, description, dueDate, priority, notes, removeTitle) {
     (this.title = title),
       (this.description = description),
       (this.dueDate = dueDate),
       (this.priority = priority),
       (this.notes = notes);
+    this.removeTitle = removeTitle;
+  }
+  removeTodo() {
+    this.removeTitle = true;
   }
 }
 
@@ -15,20 +19,25 @@ let getDescription = "Send email to John";
 let getdueDate = "Next friday";
 let getpriority = "Low";
 let getNotes = "Plan in a meeting for the report";
+let getRemoveTitle = false;
 
-function createTodo(title, description, dueDate, priority, notes) {
-  let newTodo = new CreateTodo(title, description, dueDate, priority, notes);
+function createTodo(title, description, dueDate, priority, notes, remove) {
+  let newTodo = new CreateTodo(title, description, dueDate, priority, notes, remove);
   todoList.push(newTodo);
+  return newTodo;
 }
 
-createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes);
+createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes, getRemoveTitle);
 
 getTitle = "Groceries";
 getDescription = "Buy a peach";
 getdueDate = "Next Monday";
 getpriority = "High";
 getNotes = "The peach is to make peach pie tart";
+getRemoveTitle = false;
 
-createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes);
+createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes, getRemoveTitle);
 
+console.log(todoList);
+console.log(todoList[1].removeTodo());
 console.log(todoList);
