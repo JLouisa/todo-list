9edd5f784b6dbe5f250c9977ff96e546cbf8ab01
Jlouisa <jonathan.louisa@gmail.com>
2023-08-15 13:10:28 +0200

    Rework the todo list examples

--- src/index.js
+++ src/index.js
@@ -26,6 +26,20 @@ let getpriority = "Low";
 let getNotes = "Plan in a meeting for the report";
 let getRemoveTitle = false;
 
+let getTitle2 = "Groceries";
+let getDescription2 = "Buy a peach";
+let getdueDate2 = "Next monday";
+let getpriority2 = "High";
+let getNotes2 = "The peach is to make peach pie tart";
+let getRemoveTitle2 = false;
+
+let getTitle3 = "Movie";
+let getDescription3 = "Date Night";
+let getdueDate3 = "Next saturday";
+let getpriority3 = "medium";
+let getNotes3 = "Going to go with Jay and see the new Avatar 2 movie";
+let getRemoveTitle3 = false;
+
 function createTodo(title, description, dueDate, priority, notes, remove) {
   let newTodo = new CreateTodo(title, description, dueDate, priority, notes, remove);
   todoList.push(newTodo);
@@ -33,18 +47,14 @@ function createTodo(title, description, dueDate, priority, notes, remove) {
 }
 
 createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes, getRemoveTitle);
+createTodo(getTitle2, getDescription2, getdueDate2, getpriority2, getNotes2, getRemoveTitle2);
+createTodo(getTitle3, getDescription3, getdueDate3, getpriority3, getNotes3, getRemoveTitle3);
 
-getTitle = "Groceries";
-getDescription = "Buy a peach";
-getdueDate = "Next Monday";
-getpriority = "High";
-getNotes = "The peach is to make peach pie tart";
-getRemoveTitle = false;
-
-createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes, getRemoveTitle);
-
-// console.log(todoList);
-console.log(todoList[1].removeTodo());
+//Need to be targeted to enable .removeTodo()
+console.log(todoList);
+// console.log(todoList[0].removeTodo());
+// console.log(todoList[1].removeTodo());
+console.log(todoList[2].removeTodo());
 console.log(todoList);
 
 for (item of todoList) {
