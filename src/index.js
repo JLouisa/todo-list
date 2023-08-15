db217523882c38cc24cf04c1ac1f42d205dcdaf3
Jlouisa <jonathan.louisa@gmail.com>
2023-08-15 12:12:47 +0200

    Expand the todolist variables

--- src/index.js
+++ src/index.js
@@ -3,24 +3,37 @@
 // 3. Connect array objects to html elements
 // 4. function to render array
 
-class AddTodo {
-  constructor(title, text) {
-    (this.title = title), (this.text = text);
+class CreateTodo {
+  constructor(title, description, dueDate, priority, notes) {
+    (this.title = title),
+      (this.description = description),
+      (this.dueDate = dueDate),
+      (this.priority = priority),
+      (this.notes = notes);
   }
 }
 
 let todoList = [];
 
-function createTodo(name, name2) {
-  name = new AddTodo("Shopping", "get groceries");
-  name2 = new AddTodo("Shopping2", "get groceries2");
-  todoList.push(name, name2);
+let getTitle = "Preperation";
+let getDescription = "Send email to John";
+let getdueDate = "Next friday";
+let getpriority = "Low";
+let getNotes = "Plan in a meeting for the report";
+
+function createTodo(title, description, dueDate, priority, notes) {
+  let newTodo = new CreateTodo(title, description, dueDate, priority, notes);
+  todoList.push(newTodo);
 }
 
-createTodo("todo1");
+createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes);
 
-let name3 = new AddTodo("Shopping3", "get groceries3");
-todoList.push(name3);
+getTitle = "Groceries";
+getDescription = "Buy a peach";
+getdueDate = "Next Monday";
+getpriority = "High";
+getNotes = "The peach is to make peach pie tart";
+
+createTodo(getTitle, getDescription, getdueDate, getpriority, getNotes);
 
 console.log(todoList);
-console.log(todoList[1]);
